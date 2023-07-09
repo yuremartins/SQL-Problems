e42463785173cb33b6913d46eafcad770168cc9b
Yure Martins <120994376+yuremartins@users.noreply.github.com>
2023-07-09 17:43:08 -0300

    Delete Marvel Cities.sql

--- Easy/Marvel Cities/Marvel Cities.sql
+++
@@ -1,7 +0,0 @@
--- Problem: https://www.codingninjas.com/codestudio/problems/marvel-cities_2242793?topList=top-100-sql-problems
-
-SELECT * FROM CITY
-WHERE
-Population > 100000
-AND
-CountryCode = 'Marv';
