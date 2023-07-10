74b8f5c4ca22f3fbeb9561c329f19d13df92168c
Yure Martins <comercialyure@gmail.com>
2023-07-10 12:38:38 -0300

    Create Weather Observation Station 8.sql

---
+++ SQL(Basic)/Weather Observation Station 8.sql
@@ -0,0 +1,6 @@
+-- Problem: https://www.hackerrank.com/challenges/weather-observation-station-8/problem
+
+SELECT DISTINCT(CITY) 
+FROM STATION 
+WHERE (SUBSTR(CITY, 1, 1)) IN ('a', 'e', 'i', 'o', 'u') 
+AND (SUBSTR(CITY, LENGTH(CITY), 1)) IN ('a', 'e', 'i', 'o', 'u');
